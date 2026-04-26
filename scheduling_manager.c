1e46f3522ed4ca60c51caed47595ddfb76d4caa6
Salahdine Ouhsain <souhsain@e1r8p14.1337.ma>
2026-04-26 18:45:58 +0100

    add

--- scheduling_manager.c
+++ scheduling_manager.c
@@ -1,17 +1,26 @@
 #include "mems.h"
 
-void    manage_periority(t_coder    *coder, char    *s_type, int    num_of_coders){
-    int i;
-    t_queue *periority;
+void    add_coder_acording_edf(t_coder  *coder){
+    t_queue *q;
+    t_node *tmp;
 
-    periority = malloc(sizeof(t_queue))
+    q = coder->right_dongle->priority_queue;
+    while (q && q->front->next)
+    {
+        if (q->front->value.last_compiletime > coder->last_compiletime)
+            break;
+        q = q->front->next;
+    }
+    tmp->value = q->front->value;
+    q->front->value = coder;
+    tmp->next = q->front->next;
+    q->front->next = tmp;
+    
+}
+void    manage_periority(t_coder    *coder, char    *s_type, int    num_of_coders){
     if (s_type == "edf"){
-        i = 0;
-        while (i < num_of_coders)
-        {
-            if periority
-        }
-        
+        add_coder_acording_edf(coder->right_dongle, coder->last_compiletime);
+        add_acording_edf(coder->left_dongle, coder->last_compiletime);
     }
 }
 void    add_coder_toschedual(t_coder    *coder, t_configs   cofigs){
